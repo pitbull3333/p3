a9c419a7b9be930d3cb7523e6cae3fe54d6e3dbd
Xavier THIEULEUX <123786437+pitbull3333@users.noreply.github.com>
2026-03-03 00:54:52 +0100

    conf vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/p3/client',
+  base: '/p3/',
   server: {
     port: 3000,
   },
